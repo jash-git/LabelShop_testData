eb076b96b5643e40d12cf017c63ec91f6364c757
Gitea <gitea@fake.local>
2021-06-10 11:24:52 +0800

    Create

---
+++ win-ldd_Win32/123.bat
@@ -0,0 +1,12 @@
+ldd.exe "C:\Program Files (x86)\LabelShop\LabelShop\LSLoadLsd.dll"
+pause
+ldd.exe "C:\Program Files (x86)\LabelShop\LabelShop\LabelShopEng.dll"
+pause
+ldd.exe "C:\Program Files (x86)\LabelShop\LabelShop\LabelShop.exe"
+pause
+ldd.exe "C:\Users\jashliao\Desktop\CS_TOOL\CS_Set_user-agent\CS_Set_user-agent\bin\Debug\CS_Set_user-agent.exe"
+pause
+ldd.exe "SyrisPIDCIDInput.exe"
+pause
+ldd.exe "C:\Users\jashliao\Desktop\LabelShop_testData\win-ldd_Win32\Binary\sycgaat.exe"
+pause
